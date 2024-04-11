18f6086575fc9332ba7054d67cabb5bd021761a0
layer92 <user@example.com>
2024-04-11 21:42:55 +0900

    blob to arraybuffer, text

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ export * from "./src/canvases/SetCanvasImage";
 export * from "./src/canvases/SetCanvasImageData";
 export * from "./src/client/DoesBrowserPreferDarkMode";
 export * from "./src/files/BlobToImageAsync";
+export * from "./src/files/BlobToArrayBufferAsync";
+export * from "./src/files/BlobToTextAsync";
 export * from "./src/files/MaybeMakeDownscaledImageBlobAsync";
 export * from "./src/files/PromptFileDownloadByUrl";
 export * from "./src/files/PromptFileOpen_GetFileAsync";
---
+++ src/files/BlobToArrayBufferAsync.ts
@@ -0,0 +1,13 @@
+import { Expect } from "@layer92/core";
+
+export async function BlobToArrayBufferAsync(blob:Blob){
+    return new Promise<ArrayBuffer>((accept,reject)=>{
+        const fileReader = new FileReader();
+        fileReader.onload = ()=>{
+            Expect(fileReader.result!==null,`BlobToTextAsync: fileReader result was null`,reject);
+            Expect(typeof(fileReader.result)!=="string",()=>`BlobToTextAsync: fileReader result was string instead of ArrayBuffer`,reject);
+            accept(fileReader.result);
+        }
+        fileReader.readAsArrayBuffer(blob);
+    });
+}
